55834c8fd258084a4894e4a80deff2ded204eee4
Aliaksei <alexx333by@gmail.com>
2018-11-11 20:41:53 +0300

    FileInfo class implemented

---
+++ TestsGenerator/TestsGenerator/TestFileInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TestsGenerator
+{
+    public class TestFileInfo
+    {
+        public List<string> Usings
+        { get; protected set; }
+
+        public List<TestClassInfo> Classes
+        { get; protected set; }
+    }
+}
