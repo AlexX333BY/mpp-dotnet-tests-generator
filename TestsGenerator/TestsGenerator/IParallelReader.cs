ef0812a43b1440e0411138239aec23ba81a7f0db
Aliaksei <alexx333by@gmail.com>
2018-11-11 16:26:09 +0300

    Parallel reader interface and file implementation added

---
+++ TestsGenerator/TestsGenerator/IParallelReader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TestsGenerator
+{
+    public interface IParallelReader
+    {
+        void ReadText();
+
+        IDictionary<string, string> SuccessfullyReadText
+        { get; }
+    }
+}
---
+++ TestsGenerator/TestsGenerator/ParallelFileReader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    public class ParallelFileReader : IParallelReader
+    {
+        protected readonly int maxThreadsCount;
+        protected readonly List<string> filePaths;
+        protected Dictionary<string, string> successfullyReadText;
+
+        public IDictionary<string, string> SuccessfullyReadText => new Dictionary<string, string>(successfullyReadText);
+
+        public void ReadText()
+        {
+            var result = new ConcurrentDictionary<string, string>();
+            var exceptions = new ConcurrentBag<Exception>();
+
+            ParallelOptions options = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = maxThreadsCount
+            };
+            Parallel.ForEach(filePaths, options, path =>
+                {
+                    try
+                    {
+                        result[path] = File.ReadAllText(path);
+                    }
+                    catch (Exception e)
+                    {
+                        exceptions.Add(e);
+                    }
+                }
+            );
+            successfullyReadText = new Dictionary<string, string>(result);
+
+            if (!exceptions.IsEmpty)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+
+        public ParallelFileReader(IEnumerable<string> paths, int maxThreadsCount)
+        {
+            if (maxThreadsCount < 1)
+            {
+                throw new ArgumentException("There should be at least 1 thread");
+            }
+            if (paths == null)
+            {
+                throw new ArgumentException("Paths shouldn't be null");
+            }
+
+            this.maxThreadsCount = maxThreadsCount;
+            filePaths = new List<string>(paths);
+            successfullyReadText = new Dictionary<string, string>();
+        }
+
+        public ParallelFileReader(IEnumerable<string> paths) 
+            : this(paths, Environment.ProcessorCount)
+        { }
+    }
+}
