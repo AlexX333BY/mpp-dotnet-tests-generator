91185fcc9f975274be085c7637bda8b538db9077
Aliaksei <alexx333by@gmail.com>
2018-11-11 20:37:10 +0300

    ClassInfo class improved

--- TestsGenerator/TestsGenerator/TestClassInfo.cs
+++ TestsGenerator/TestsGenerator/TestClassInfo.cs
@@ -11,8 +11,12 @@ namespace TestsGenerator
         public string Namespace
         { get; protected set; }
 
-        public TestClassInfo(string @namespace)
+        public string Name
+        { get; protected set; }
+
+        public TestClassInfo(string name, string @namespace)
         {
+            Name = name ?? throw new ArgumentException("Name shouldn't be null");
             Namespace = @namespace ?? throw new ArgumentException("Namespace shouldn't be null");
             Methods = new List<TestMethodInfo>();
         }
