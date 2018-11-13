82c1471c68940eb452f7342d0172b429f69039cc
Aliaksei <alexx333by@gmail.com>
2018-11-13 13:39:09 +0300

    UsageExample implemented

---
+++ TestsGenerator/TestsGenerator.UsageExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using TestsGenerator.IO;
+
+namespace TestsGenerator.UsageExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestsGeneratorConfig config = new TestsGeneratorConfig
+            {
+                ReadPaths = new List<string>
+                {
+                    "HelloWorld.cs",
+                    "CRefTest.cs"
+                },
+                Writer = new FileWriter()
+                {
+                    Directory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+                },
+                ReadThreadCount = 2,
+                WriteThreadCount = 2
+            };
+
+            new TestsGenerator(config).Generate();
+        }
+    }
+}
