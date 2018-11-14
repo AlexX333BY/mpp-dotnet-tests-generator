1e0849610f53519bb5a7b08ccc84500313f89a81
Aliaksei <alexx333by@gmail.com>
2018-11-14 15:10:08 +0300

    Some unit tests implemented

---
+++ TestsGenerator/TestsGenerator.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestsGenerator.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestsGenerator.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f28e0c6d-b46f-4df6-bece-c71a8c68d7e2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestsGenerator/TestsGenerator.UnitTests/TestsGeneratorUnitTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestsGenerator.IO;
+
+namespace TestsGenerator.UnitTests
+{
+    [TestClass]
+    public class TestsGeneratorUnitTests
+    {
+        private CompilationUnitSyntax class1Root, class2Root;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            string testFilesDirectory = "../../";
+            string testFilePath = testFilesDirectory + "TestFile.cs";
+            string testClass1FilePath = testFilesDirectory + "TestClass1Test.cs";
+            string testClass2FilePath = testFilesDirectory + "TestClass2Test.cs";
+
+            TestsGeneratorConfig config = new TestsGeneratorConfig
+            {
+                ReadPaths = new List<string>
+                {
+                    testFilePath
+                },
+                Writer = new FileWriter()
+                {
+                    Directory = testFilesDirectory
+                }
+            };
+
+            new TestsGenerator(config).Generate();
+
+            class1Root = CSharpSyntaxTree.ParseText(File.ReadAllText(testClass1FilePath)).GetCompilationUnitRoot();
+            class2Root = CSharpSyntaxTree.ParseText(File.ReadAllText(testClass2FilePath)).GetCompilationUnitRoot();
+        }
+
+        [TestMethod]
+        public void UsingTests()
+        {
+            Assert.IsTrue(class1Root.Usings.Any((usingEntry) => usingEntry.Name.ToString() == "Moq"));
+            Assert.IsTrue(class1Root.Usings.Any((usingEntry) => usingEntry.Name.ToString() == "System"));
+            Assert.IsTrue(class1Root.Usings.Any((usingEntry) => usingEntry.Name.ToString() == "TestClassNamespace.Class1"));
+            Assert.IsTrue(class2Root.Usings.Any((usingEntry) => usingEntry.Name.ToString() == "TestClassNamespace.Class2"));
+        }
+
+        [TestMethod]
+        public void NamespaceTest()
+        {
+            IEnumerable<NamespaceDeclarationSyntax> namespaces;
+
+            namespaces = class1Root.DescendantNodes().OfType<NamespaceDeclarationSyntax>();
+            Assert.AreEqual(1, namespaces.Count());
+            Assert.AreEqual("TestClassNamespace.Class1.Test", namespaces.First().Name.ToString());
+
+            namespaces = class2Root.DescendantNodes().OfType<NamespaceDeclarationSyntax>();
+            Assert.AreEqual(1, namespaces.Count());
+            Assert.AreEqual("TestClassNamespace.Class2.Test", namespaces.First().Name.ToString());
+        }
+
+        [TestMethod]
+        public void ClassTest()
+        {
+            IEnumerable<ClassDeclarationSyntax> classes;
+
+            classes = class1Root.DescendantNodes().OfType<ClassDeclarationSyntax>();
+            Assert.AreEqual(1, classes.Count());
+            Assert.AreEqual("TestClass1Test", classes.First().Identifier.ToString());
+
+            classes = class2Root.DescendantNodes().OfType<ClassDeclarationSyntax>();
+            Assert.AreEqual(1, classes.Count());
+            Assert.AreEqual("TestClass2Test", classes.First().Identifier.ToString());
+        }
+    }
+}
